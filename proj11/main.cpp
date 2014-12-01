48b5dbe6ce54ae4b8b3d5f9a7c41e44880b90955
Bryan J Kars <kars@Bryans-MacBook-Pro.local>
2014-11-30 19:22:06 -0500

    Initial Commit

---
+++ proj11/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  proj11
+//
+//  Created by Bryan J Kars on 11/30/14.
+//  Copyright (c) 2014 Bryan J Kars. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
